9bf36b2bb466da5a2d0d2353d8336e634678f298
Ilya Samsonov <ias78@tpu.ru>
2024-05-22 01:38:52 +0700

    add function title_word and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def upp_word(word):
+    """
+    Возвращает слово с заглавными буквами
+    """
+
     return word.upper()
+
+def title_word(word):
+    """
+    Возвращает строку с первыми
+    заглавными буквами
+    """
+
+    return word.title()
